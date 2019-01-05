fcf57071dc924b466021a858656d7d9249b09e6e
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-01-05 17:02:26 -0500

    DishDetail_Component

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Media } from 'reactstrap';
+
+class DishDetail extends Component {
+
+   
+    renderDish(dish) {
+        if (dish != null)
+            return (
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return (
+                <div></div>
+            );
+    }
+
+    renderComments(commentsArg) {
+        if (commentsArg != null) {
+            const comments = commentsArg.map((comment) => {
+                return (
+                 
+                    <Media list key={comment.id} className="list-unstyled">
+                        <Media tag="li">
+                      
+                            <p>{comment.comment}</p>
+                        </Media>
+                        <Media tag="li">
+                            -- {comment.author}, {new Intl.DateTimeFormat('en-US',
+                                { year: 'numeric', month: 'long', day: '2-digit' }).format(new Date(comment.date))}
+                                              
+                        </Media>
+                    </Media>
+                                          
+                                            
+                );
+            });
+            return (
+                <div>
+                
+                    <Media>
+                        <h4>Comments</h4>
+                    </Media>
+                    {comments}
+                </div>
+            );
+        } else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    render() {
+
+        let comments;
+        if (this.props.dish != null) {
+            comments = this.renderComments(this.props.dish.comments);
+        }
+
+        return (
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.dish)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    {comments}
+                </div>
+            </div>
+        );
+    }
+}
+export default DishDetail;
