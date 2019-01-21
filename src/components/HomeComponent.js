66a006259f0cf4391d248c79200a982f102a483c
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-01-21 13:04:54 -0500

    Fetch Handling Errors

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,33 +1,42 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 
-function RenderCard({item, isLoading, errMess}) {
- if (isLoading) {
-        return(
-                <Loading />
-        );
-    }
-    else if (errMess) {
-        return(
-                <h4>{errMess}</h4>
-        );
-    }
-    else 
-        return(
-            <Card>
-                <CardImg src={baseUrl + item.image} alt={item.name} />
-                <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
-                <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
-        );
 
-}
+function RenderCard({item, isLoading, errMess}) {
+     if (isLoading) {
+            return(
+                    <Loading />
+            );
+        }
+        else if (errMess) {
+            return(
+                    <h4>{errMess}</h4>
+            );
+        }
+        else {
+            return(
+                <Card>
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardBody>
+                        <CardTitle>
+                            {item.name}
+                        </CardTitle>
+                       
+                            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+
+                        <CardText>
+                            {item.description}
+                        </CardText>
+                    </CardBody>
+                </Card>
+            );
+
+       }
+     }
+
+    
 
 function Home(props) {
     return(
@@ -37,7 +46,8 @@ function Home(props) {
                     
                     <RenderCard 
                         item={props.dish} 
-                        isLoading={props.dishesLoading} errMess={props.dishesErrMess}  
+                        isLoading={props.dishesLoading} 
+                        errMess={props.dishesErrMess}  
                     />
                 </div>
                  <div className="col-12 col-md m-1">
@@ -46,7 +56,8 @@ function Home(props) {
                 <div className="col-12 col-md m-1">
                     <RenderCard 
                         item={props.promotion} 
-                        isLoading={props.promoLoading} errMess={props.promoErrMess} 
+                        isLoading={props.promoLoading} 
+                        errMess={props.promoErrMess} 
                     />
                 </div>
                
